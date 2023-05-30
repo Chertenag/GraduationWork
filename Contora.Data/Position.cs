b3b0bb7b119f816457d2e111eff06e754801a042
Chertenag <chertenag@gmail.com>
2023-05-30 12:10:08 +0300

    Добавьте файлы проекта.

---
+++ Contora.Data/Position.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Contora.Data;
+
+public partial class Position
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Agent> Agents { get; set; } = new List<Agent>();
+}
---
+++ Contora.Data/Rank.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Contora.Data;
+
+public partial class Rank
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Agent> Agents { get; set; } = new List<Agent>();
+}
